598ba5ac038bddc9b24ae35dd3c160925788bb37
gnu11111 <gnu@gnu.at>
2023-01-26 16:56:54 +0100

    Day 21 - Monkey Math

---
+++ src/main/kotlin/at/gnu/adventofcode/year2022/Day21.kt
@@ -0,0 +1,125 @@
+package at.gnu.adventofcode.year2022
+
+class Day21(input: List<String>) {
+
+    companion object {
+        const val resource = "/adventofcode/year2022/Day21.txt"
+        val line = """(.*): (.*)""".toRegex()
+        const val rootName = "root"
+        const val humanName = "humn"
+    }
+
+    data class Monkey(val name: String, var value: Long? = null, val monkey1: String? = null,
+                      val operation: Char? = null, val monkey2: String? = null)
+
+    private val root: Monkey
+    private val human: Monkey
+    private val monkeys = input.map {
+        val (name, rest) = line.matchEntire(it)!!.destructured
+        val value = rest.toLongOrNull()
+        if (value == null) {
+            val (monkey1, operation, monkey2) = rest.split(" ")
+            Monkey(name, null, monkey1, operation[0], monkey2)
+        } else
+            Monkey(name, value)
+    }
+
+    init {
+        root = monkeys.getMonkey(rootName)
+        human = monkeys.getMonkey(humanName)
+    }
+
+
+    fun part1(): Long =
+        root.fillValues().value!!
+
+    fun part2(): Long {
+
+        root.resetValues().fillValues()
+        val monkeysFromHumanToRoot = mutableListOf<Monkey>()
+        var monkey = human
+        while (monkey != root) {
+            monkey = monkeys.first { it.monkey1 == monkey.name || it.monkey2 == monkey.name }
+            monkeysFromHumanToRoot += monkey
+        }
+
+        val fix: String
+        val variable: String
+        if (monkeysFromHumanToRoot.last().monkey1 == monkeysFromHumanToRoot.dropLast(1).last().name) {
+            fix = monkeysFromHumanToRoot.last().monkey2!!
+            variable = monkeysFromHumanToRoot.last().monkey1!!
+        } else {
+            fix = monkeysFromHumanToRoot.last().monkey1!!
+            variable = monkeysFromHumanToRoot.last().monkey2!!
+        }
+        val fixValue = monkeys.getMonkey(fix).value!!
+
+        var lo = 0L
+        var hi = 1_000_000_000_000_000L
+        var mid = 0L
+        while ((lo + 1) < hi) {
+            mid = (lo + hi) / 2L
+            root.resetValues().fillValues(mid)
+            val value = monkeys.getMonkey(variable).value!!
+            val score = fixValue - value
+//          println("$mid $lo $hi $fixValue $value $score")
+            if (score < 0L)
+                lo = mid
+            else if (score == 0L)
+                break
+            else
+                hi = mid
+        }
+
+//        println(monkeys.getMonkey(fix).value!!)
+//        println(monkeys.getMonkey(variable).value!!)
+//
+//        for (i in 0..10) {
+//            root.resetValues().fillValues(3782852515580L + i)
+//            println("${3782852515580L + i} ${monkeys.getMonkey(root.monkey1).value} ${monkeys.getMonkey(root.monkey2).value}")
+//        }
+
+        return mid
+    }
+
+
+    private fun Monkey.resetValues(): Monkey {
+        for (monkey in monkeys)
+            if ((monkey.monkey1 != null) && (monkey.monkey2 != null))
+                monkey.value = null
+        return this
+    }
+
+    private fun Monkey.fillValues(humanValue: Long? = null): Monkey {
+        while (value == null) {
+            for (monkey in monkeys) {
+                if (monkey.value != null)
+                    continue
+                val value1 = if ((monkey.monkey1 == humanName) && (humanValue != null)) humanValue else monkeys.getMonkey(monkey.monkey1).value
+                val value2 = if ((monkey.monkey2 == humanName) && (humanValue != null)) humanValue else monkeys.getMonkey(monkey.monkey2).value
+                if ((value1 != null) && (value2 != null))
+                    monkey.value = calculateExpression(value1, monkey.operation!!, value2)
+            }
+        }
+        return this
+    }
+
+    private fun calculateExpression(value1: Long, operation: Char, value2: Long) =
+        when (operation) {
+            '+' -> value1 + value2
+            '-' -> value1 - value2
+            '*' -> value1 * value2
+            '/' -> value1 / value2
+            else -> error("unknown operation $operation!")
+        }
+
+    private fun List<Monkey>.getMonkey(name: String?): Monkey =
+        if (name != null) first { it.name == name } else Monkey("????")
+}
+
+fun main() {
+    val input = Day21::class.java.getResource(Day21.resource)!!.readText().trim().split("\n", "\r\n")
+    val day21 = Day21(input)
+    println("Day21::part1 -> ${day21.part1()}")
+    println("Day21::part2 -> ${day21.part2()}") // 3782852515583
+}
---
+++ src/test/kotlin/at/gnu/adventofcode/year2022/Day21Test.kt
@@ -0,0 +1,42 @@
+package at.gnu.adventofcode.year2022
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
+
+class Day21Test {
+
+    private val monkeys = """
+        root: pppw + sjmn
+        dbpl: 5
+        cczh: sllz + lgvd
+        zczc: 2
+        ptdq: humn - dvpt
+        dvpt: 3
+        lfqf: 4
+        humn: 5
+        ljgn: 2
+        sjmn: drzm * dbpl
+        sllz: 4
+        pppw: cczh / lfqf
+        lgvd: ljgn * ptdq
+        drzm: hmdt - zczc
+        hmdt: 32
+    """.trimIndent().split("\n")
+
+    private val test = mapOf(
+        Day21::part1 to 152L,
+        Day21::part2 to 301L
+    )
+
+    @Test @ExperimentalTime
+    fun testMySolution() {
+        val day21 = Day21(monkeys)
+        for (function in test.keys) {
+            val (result, time) = measureTimedValue { function(day21) }
+            println("Day21::${function.name}: ${monkeys.size} monkeys -> $result [$time]")
+            assertEquals(test[function], result)
+        }
+    }
+}
